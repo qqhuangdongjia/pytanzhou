c0add84ab782cb508189b2128d4a67959112a2d2
qqhuangdongjia <38077152+qqhuangdongjia@users.noreply.github.com>
2018-05-21 20:41:42 +0800

    1.0

--- tanzhou/settings.py
+++ tanzhou/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tanzhou.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tanzhou',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'qwe123',
     }
 }
 
